270e9e79df92bc809d70327ef8cfd0c92707bc8c
Simon T <Simon T@DESKTOP-IM0OI1B>
2019-09-05 09:30:49 +0200

    Changes to X/Y-axle, Move method

--- Labb4/Program.cs
+++ Labb4/Program.cs
@@ -3,26 +3,38 @@ using System.Collections.Generic;
 
 namespace Labb4
 {
+    // class Tile contains representation (char) holds just one sign.
+    // bool accessible to check if it's accessible later on, meaning if you can go to its position
+    // List<objects> contents. Wilhelm, wut is dis? Example, object Key?
     public class Tile
     {
         public char representation;
         public bool accessible;
         public List<object> contents;
     }
+
     public class RoomTile : Tile
-    {// Provar commit
-        public RoomTile() { this.representation = '.'; }
-        public RoomTile(char representation)
+    {
+        public RoomTile()
         {
-            this.representation = representation;
-            this.accessible = true;
+            this.representation = '.';
+            this.accessible = true; // accessilbe Originates to the RoomTile(char representation) constructor below.
         }
+
+        // Leave this for now, not needed as of current version
+        //public RoomTile(char representation) // public constructor taking the representation as an argument and sets the representation of this class to the representation??
+        //{                                   // this.representation is not needed, works anyway. this.accessible = true, sets this '.' to accessible.
+        //    // this.representation = representation;
+        //    this.accessible = true;
+        //}
     }
+
     public class DoorTile : Tile
     {
         public DoorTile()
         {
-            this.representation = 'D';
+            this.representation = 'D'; // sets the char representation to the char 'D' in this constructor
+            //this.accessible = ? Key needed? If so, make method call to check if key collected. If collected, set accessible to true, otherwise false until key is collected.
         }
     }
     public class WallTile : Tile
@@ -38,26 +50,48 @@ namespace Labb4
     {
         public int PosX;
         public int PosY;
-        public Tile currentTile;
-        private int lives;
-        public int Lives { get => lives; set => lives = value; }
-
-        enum Dir { Up, Down, Left, Right }
-
-        public void moveUp() { if (CanMove(Dir.Up)) this.PosY++; }
-        public void moveDown() { if (CanMove(Dir.Down)) this.PosX++; }
-        public void moveLeft() { if (CanMove(Dir.Left)) this.PosY--; }
-        public void moveRight() { if (CanMove(Dir.Right)) this.PosY++; }
-        private bool CanMove(Player.Dir d)
-        {
-            //TODO: logic
-            return true;
-        }
         public Player(int posX, int posY, int lives)
         {
             this.PosX = posX;
             this.PosY = posY;
             this.Lives = lives;
+        }
+        private int lives;
+        public int Lives { get => lives; set => lives = value; }
+        enum Dir { Up, Down, Left, Right }
+
+        // Movement methods, if (CanMove(enum Dir.member)) change the value of the position variable and Redraws the map
+        // Y-line has inverted values. Up = Y--, while in math Up is Y++
+        public void moveUp(Tile t) { if (CanMove(Dir.Up)) this.PosY--; } // originally PosY++;
+        public void moveDown() { if (CanMove(Dir.Down)) this.PosY++; } // originally PosX++;
+        public void moveLeft() { if (CanMove(Dir.Left)) this.PosX--; } // originally PosY--;
+        public void moveRight() { if (CanMove(Dir.Right)) this.PosX++; } // originally PoSY++;
+        private bool CanMove(Player.Dir d)
+        {
+            switch (d)
+            {
+                case Dir.Up:
+                    if ((t.)
+                        return false;
+                    else
+                        return true;
+                case Dir.Down:
+                    if (PosY + 1.Equals())
+                        return false;
+                    else
+                        return true;
+                case Dir.Left:
+                    if (PosX.Equals("#"))
+                        return false;
+                    else
+                        return true;
+                case Dir.Right:
+                    if (PosX.Equals("#"))
+                        return false;
+                    else
+                        return true;
+            }
+            return true;
         }
     }
 
@@ -65,7 +99,7 @@ namespace Labb4
     {
         const int mapX = 10;
         const int mapY = 10;
-        Tile[,] map = new Tile[10, 10];
+        Tile[,] map = new Tile[mapX, mapY];
         Player player = new Player(5, 5, 10);
 
         public void Start()
@@ -81,10 +115,10 @@ namespace Labb4
                 }
             }
             while (true)
-            {
+            { 
                 Console.Clear();
                 Draw();
-                switch(Console.ReadKey().Key)
+                switch (Console.ReadKey().Key)
                 {
                     case ConsoleKey.W:
                         player.moveUp();
@@ -98,6 +132,9 @@ namespace Labb4
                     case ConsoleKey.D:
                         player.moveRight();
                         break;
+                    case ConsoleKey.Escape:
+                        Environment.Exit(0);
+                        break;
                 }
             }
         }
@@ -107,8 +144,8 @@ namespace Labb4
             {
                 for (int j = 0; j < mapY; j++)
                 {
-                    char c = (player.PosX == i && player.PosY == j) ? 'X' : map[i, j].representation;
-                    Console.Write(c+ " ");
+                    char c = (player.PosX == j && player.PosY == i) ? 'X' : map[i, j].representation;
+                    Console.Write(c + " ");
                 }
                 Console.WriteLine();
             }
