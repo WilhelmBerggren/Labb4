c0f08d37284e410a8b1860d5a79f6d96b2ef0557
Simon T <Simon T@DESKTOP-IM0OI1B>
2019-09-05 11:45:32 +0200

    Adding monster and fixing movement methods

--- Labb4/Program.cs
+++ Labb4/Program.cs
@@ -1,16 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace Labb4
 {
     // class Tile contains representation (char) holds just one sign.
-    // bool accessible to check if it's accessible later on, meaning if you can go to its position
-    // List<objects> contents. Wilhelm, wut is dis? Example, object Key?
+    // bool accessible to check if it's accessible to step on, meaning if you can go to its position
     public class Tile
     {
         public char representation;
         public bool accessible;
-        public List<object> contents;
     }
 
     public class RoomTile : Tile
@@ -18,25 +15,19 @@ namespace Labb4
         public RoomTile()
         {
             this.representation = '.';
-            this.accessible = true; // accessilbe Originates to the RoomTile(char representation) constructor below.
+            this.accessible = true;
         }
-
-        // Leave this for now, not needed as of current version
-        //public RoomTile(char representation) // public constructor taking the representation as an argument and sets the representation of this class to the representation??
-        //{                                   // this.representation is not needed, works anyway. this.accessible = true, sets this '.' to accessible.
-        //    // this.representation = representation;
-        //    this.accessible = true;
-        //}
     }
 
     public class DoorTile : Tile
     {
         public DoorTile()
         {
-            this.representation = 'D'; // sets the char representation to the char 'D' in this constructor
-            //this.accessible = ? Key needed? If so, make method call to check if key collected. If collected, set accessible to true, otherwise false until key is collected.
+            this.representation = 'D';
+            this.accessible = true;
         }
     }
+
     public class WallTile : Tile
     {
         public WallTile()
@@ -46,91 +37,64 @@ namespace Labb4
         }
     }
 
+    public class MonsterTile : Tile
+    {
+        public MonsterTile()
+        {
+            this.representation = 'M';
+            this.accessible = true;
+        }
+    }
+
     public class Player
     {
         public int PosX;
         public int PosY;
+        public int lives;
         public Player(int posX, int posY, int lives)
         {
             this.PosX = posX;
             this.PosY = posY;
-            this.Lives = lives;
+            this.lives = lives;
         }
-        private int lives;
-        public int Lives { get => lives; set => lives = value; }
-        enum Dir { Up, Down, Left, Right }
 
-        // Movement methods, if (CanMove(enum Dir.member)) change the value of the position variable and Redraws the map
-        // Y-line has inverted values. Up = Y--, while in math Up is Y++
-        public void moveUp(Tile t) { if (CanMove(Dir.Up)) this.PosY--; } // originally PosY++;
-        public void moveDown() { if (CanMove(Dir.Down)) this.PosY++; } // originally PosX++;
-        public void moveLeft() { if (CanMove(Dir.Left)) this.PosX--; } // originally PosY--;
-        public void moveRight() { if (CanMove(Dir.Right)) this.PosX++; } // originally PoSY++;
-        private bool CanMove(Player.Dir d)
+        public void move(Tile t, int deltaX, int deltaY)
         {
-            switch (d)
+            if (t.accessible)
             {
-                case Dir.Up:
-                    if ((t.)
-                        return false;
-                    else
-                        return true;
-                case Dir.Down:
-                    if (PosY + 1.Equals())
-                        return false;
-                    else
-                        return true;
-                case Dir.Left:
-                    if (PosX.Equals("#"))
-                        return false;
-                    else
-                        return true;
-                case Dir.Right:
-                    if (PosX.Equals("#"))
-                        return false;
-                    else
-                        return true;
+                this.PosX += deltaX;
+                this.PosY += deltaY;
+                if (t.GetType() == typeof(MonsterTile)) lives--;
             }
-            return true;
         }
     }
 
+
     public class Game
     {
-        const int mapX = 10;
-        const int mapY = 10;
-        Tile[,] map = new Tile[mapX, mapY];
+        const int mapHeight = 10;
+        const int mapWidth = 10;
+        Level level = new Level(mapHeight, mapWidth);
         Player player = new Player(5, 5, 10);
 
         public void Start()
         {
-            for (int x = 0; x < map.GetLength(0); x++)
-            {
-                for (int y = 0; y < map.GetLength(1); y++)
-                {
-                    if (x == 0 || y == 0 || x == mapX - 1 || y == mapY - 1)
-                        map[x, y] = new WallTile();
-                    else
-                        map[x, y] = new RoomTile();
-                }
-            }
             while (true)
-            { 
-                Console.Clear();
+            {
                 Draw();
                 switch (Console.ReadKey().Key)
                 {
                     case ConsoleKey.W:
-                        player.moveUp();
+                        player.move(level.map[player.PosX - 1, player.PosY], -1, 0);
                         break;
                     case ConsoleKey.A:
-                        player.moveLeft();
+                        player.move(level.map[player.PosX, player.PosY - 1], 0, -1);
                         break;
                     case ConsoleKey.S:
-                        player.moveDown();
+                        player.move(level.map[player.PosX + 1, player.PosY], +1, 0);
                         break;
                     case ConsoleKey.D:
-                        player.moveRight();
+                        player.move(level.map[player.PosX, player.PosY + 1], 0, +1);
                         break;
                     case ConsoleKey.Escape:
                         Environment.Exit(0);
@@ -140,17 +104,49 @@ namespace Labb4
         }
         public void Draw()
         {
-            for (int i = 0; i < mapX; i++)
+            Console.Clear();
+            for (int x = 0; x < mapWidth; x++)
             {
-                for (int j = 0; j < mapY; j++)
+                for (int y = 0; y < mapHeight; y++)
                 {
-                    char c = (player.PosX == j && player.PosY == i) ? 'X' : map[i, j].representation;
+                    char c = (player.PosX == x && player.PosY == y) ? 'X' : level.map[x, y].representation;
                     Console.Write(c + " ");
                 }
                 Console.WriteLine();
             }
-            Console.WriteLine($"lives: {player.Lives}");
+            Console.WriteLine($"lives: {player.lives}");
         }
+    }
+
+    class Level
+    {
+        public Tile[,] map;
+        public int mapHeight;
+        public int mapWidth;
+        public Level(int mapHeight, int mapWidth)
+        {
+            this.mapHeight = mapHeight;
+            this.mapWidth = mapWidth;
+            this.map = new Tile[mapHeight, mapWidth];
+            setTiles();
+        }
+
+        void setTiles()
+        {
+            for (int x = 0; x < this.mapWidth; x++)
+            {
+                for (int y = 0; y < this.mapHeight; y++)
+                {
+                    if (x == 3 && y == 4)
+                        map[x, y] = new MonsterTile();
+                    else if (x == 0 || y == 0 || x == this.mapHeight - 1 || y == this.mapWidth - 1)
+                        map[x, y] = new WallTile();
+                    else
+                        map[x, y] = new RoomTile();
+                }
+            }
+        }
+
     }
 
     class Program
